fd55d4da95fd911908a7b4b975adb135d21f2787
wisdomm11 <gurojihoon@gmail.com>
2025-07-25 11:18:44 +0900

    jwt토큰필터 수정 -김지훈-

---
+++ music/src/main/java/com/music/common/auth/JwtTokenFilter2.java
@@ -0,0 +1,75 @@
+package com.music.common.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class JwtTokenFilter2 extends OncePerRequestFilter {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain) throws ServletException, IOException {
+        try {
+            String token = request.getHeader("Authorization");
+
+            if (token != null) {
+                if (!token.substring(0, 7).equals("Bearer ")) {
+                    throw new AuthenticationServiceException("Bearer 형식 아닙니다.");
+                }
+                String jwtToken = token.substring(7);
+
+                Claims claims = Jwts.parserBuilder()
+                        .setSigningKey(secretKey)
+                        .build()
+                        .parseClaimsJws(jwtToken)
+                        .getBody();
+
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + claims.get("role")));
+                UserDetails userDetails = new User(claims.getSubject(), "", authorities);
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails, jwtToken, userDetails.getAuthorities());
+
+                // 어덴티케이션에 리퀘스트 정보를 넣는것
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            response.getWriter().write("invalid token");
+        }
+    }
+
+
+
+}
